ecab30d9fe7ba3eb871d00b7b853a50fc1ab4cf0
firstChairCoder <md1qq7@gmail.com>
2022-12-04 07:15:12 +0100

    chore(font): add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Montserrat_300Light,
+  Montserrat_400Regular,
+  Montserrat_700Bold
+} from "@expo-google-fonts/montserrat";
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    MontserratLight: Montserrat_300Light,
+    MontserratRegular: Montserrat_400Regular,
+    MontserratBold: Montserrat_700Bold
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="inverted" />
     </View>
   );
 }
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
